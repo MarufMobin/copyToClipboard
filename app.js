2b5e59c2073e37bee74eda8de7cfae8bf7d8030d
unknown <mobinh264@gmail.com>
2021-08-13 20:07:37 +0600

    Copy clickClipBoard

---
+++ app.js
@@ -0,0 +1,9 @@
+const input = document.querySelector('#input');
+const button = document.querySelector('button.btn');
+
+button.onclick = function(){
+    input.select();
+    document.execCommand('copy');
+
+    alert('Coppied');
+}
